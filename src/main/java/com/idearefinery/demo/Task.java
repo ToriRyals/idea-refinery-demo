1fd6db668d9c7ce2829e715673e50cc9c36a3c67
Tori <victoriaryals@gmail.com>
2023-10-30 15:17:21 -0400

    Task class creation

---
+++ src/main/java/com/idearefinery/demo/Task.java
@@ -0,0 +1,51 @@
+package com.idearefinery.demo;
+import java.util.Date;
+
+public class Task {
+    private String title;
+    private String description;
+    private boolean completed;
+    private Date createDate;
+    private Date completedDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getCompletedDate() {
+        return completedDate;
+    }
+
+    public void setCompletedDate(Date completedDate) {
+        this.completedDate = completedDate;
+    }
+}
+
